e70b304b3bc4c3e9f5cd09478bfa38720c26874e
Daniel Ceballos <daniel.ceballos.arvizu@gmail.com>
2023-10-18 22:30:39 -0600

    Crear tests en lib en lugar de main
    Se eliminó el código de main para usarlo en tests en lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,24 @@
 pub mod calculadora;
 pub mod figuras;
+
+#[cfg(test)]
+mod tests {
+    use crate::calculadora::Calculadora;
+    use crate::figuras::circulo::Circulo;
+    use crate::figuras::cuadrado::Cuadrado;
+    use crate::figuras::triangulo::Triangulo;
+    #[test]
+    fn calculo_de_areas() {
+        let mut prueba: Calculadora = Calculadora::new();
+
+        prueba.nuevo_elemento(Circulo::new(1.0));
+
+        prueba.nuevo_elemento(Cuadrado::new(5.0));
+
+        prueba.nuevo_elemento(Triangulo::new(23.0, 32.0, 56.0, 78.0));
+
+        assert_eq!(prueba.obtener_area(0).unwrap(), std::f64::consts::PI);
+        assert_eq!(prueba.obtener_area(1).unwrap(), 25.0);
+        assert_eq!(prueba.obtener_area(2).unwrap(), 897.0);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,6 @@
-// ==========================================================================
-//                       Main
-//===========================================================================
-
 use calculadora_geometrica::calculadora::Calculadora;
 use calculadora_geometrica::figuras::circulo::Circulo;
 use calculadora_geometrica::figuras::cuadrado::Cuadrado;
 use calculadora_geometrica::figuras::triangulo::Triangulo;
 
-fn main() {
-    println!("Hello 🌍!");
-
-    let mut prueba: Calculadora = Calculadora::new();
-
-    prueba.nuevo_elemento(Circulo::new(23.1));
-
-    prueba.nuevo_elemento(Cuadrado::new(24.5));
-
-    prueba.nuevo_elemento(Triangulo::new(23.0, 32.0, 56.0, 78.0));
-
-    prueba.nuevo_elemento(Triangulo::from((23.0, 32.0, 56.0, 78.0)));
-
-    prueba.mostrar_figuras();
-
-    println!("{:?}", prueba.obtener_area(0));
-    println!("{:?}", prueba.obtener_area(1));
-    println!("{:?}", prueba.obtener_area(2));
-    println!("{:?}", prueba.obtener_area(3));
-}
+fn main() {}
